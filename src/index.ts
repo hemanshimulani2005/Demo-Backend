e8752c42d74b00bb5e1f08d244ba8b39ab7d4fb9
hemanshimulani05 <hemanshimulani@gmail.com>
2025-09-05 00:42:19 +0530

    intialize project

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express, { Application, Request, Response } from "express";
+import dotenv from "dotenv";
+// import userRoutes from "./routes/user.routes";
+
+dotenv.config();
+
+const app: Application = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+// Routes
+// app.use("/api/users", userRoutes);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello TypeScript + Node.js Backend 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});

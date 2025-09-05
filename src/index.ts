4934b58b76b723c290a82a4b5177e618c6881f73
kananimaulik26 <kananimaulik26@gmail.com>
2025-09-05 13:28:59 +0530

    abc

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,22 @@
-import express, { Application, Request, Response } from "express";
-import dotenv from "dotenv";
-// import userRoutes from "./routes/user.routes";
+// // import express, { Application, Request, Response } from "express";
+// // import dotenv from "dotenv";
+// // import userRoutes from "./routes/user.routes";
 
-dotenv.config();
+// // dotenv.config();
 
-const app: Application = express();
-const PORT = process.env.PORT || 5000;
+// // const app: Application = express();
+// // const PORT = process.env.PORT || 5000;
+// //
+// app.use(express.json());
 
-app.use(express.json());
+// // Routes
+// //com
+// // app.use("/api/users", userRoutes);
 
-// Routes
-// app.use("/api/users", userRoutes);
+// app.get("/", (req: Request, res: Response) => {
+//   res.send("Hello TypeScript + Node.js Backend 🚀");
+// });
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello TypeScript + Node.js Backend 🚀");
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
